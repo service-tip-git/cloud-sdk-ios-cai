ba6cf23ab19bc88957b3d7f2baa109709deb79fc
XiaoYu <33440079+ShadowTourist@users.noreply.github.com>
2021-09-04 04:28:24 +0800

    feat: 🎸 gif supported through SDWebImage (#32)
    * feat: 🎸 gif supported by SDWebImage.
    
    * feat: 🎸 placeholder and failure support alse README updated
    
    * docs: ✏️ update README

--- Sources/SAPCAI/UI/Common/SwiftUI/CarouselImageView.swift
+++ Sources/SAPCAI/UI/Common/SwiftUI/CarouselImageView.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import URLImage
 
 struct CarouselImageView: View {
     var media: MediaItem?
@@ -13,18 +12,11 @@ struct CarouselImageView: View {
     var body: some View {
         Group {
             if let mediaItem = media, let sourceUrl = mediaItem.sourceUrl {
-                URLImage(url: sourceUrl,
-                         inProgress: { _ -> Image in
-                             mediaItem.placeholder
-                         },
-                         failure: { error, _ in
-                             Text(error.localizedDescription)
-                         },
-                         content: { image in
-                             image
-                                 .resizable()
-                                 .aspectRatio(contentMode: .fill)
-                         })
+                ImageViewWrapper(url: sourceUrl,
+                                 placeholder: { mediaItem.placeholder },
+                                 failure: { Text($0.localizedDescription) },
+                                 content: { $0 })
+                    .scaledToFill()
                     .frame(width: self.itemWidth, height: self.vSizeClass == .regular ? 180 : 80)
                     .clipped()
             } else {
